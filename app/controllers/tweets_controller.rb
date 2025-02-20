ea1cebf66276565a30f492a2b720b09620e6008a
yasuhito1105 <yatti10@gmail.com>
2025-02-20 12:38:15 +0900

    new、createアクションの追加　バリデーションの追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,19 @@
 class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all
-    
+  end
+
+  def new
+    @tweet = Tweet.all
+  end
+
+  def create
+    Tweet.create(tweet_params)
+    redirect_to '/'
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:name, :image, :text)
   end
 end

c4958b966e909942c8ab908ffda3f4f916e00d83
yasuhito1105 <yatti10@gmail.com>
2025-02-28 20:10:53 +0900

    ユーザーのマイページの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-    def show
-      @nickname = current_user.nickname
-      @tweets = current_user.tweets
-    end
+  def show
+    user = User.find(params[:id])
+    @nickname = user.nickname
+    @tweets = user.tweets
+  end
 end

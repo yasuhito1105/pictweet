b3798affbac9073fa337bc16634d21e3ea7bcb34
yasuhito1105 <yatti10@gmail.com>
2025-03-05 11:33:13 +0900

    コメント機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to:'tweets#index'
-  resources :tweets
+  root to: 'tweets#index'
+  resources :tweets do
+    resources :comments, only: :create
+  end
   resources :users, only: :show
 end
-  

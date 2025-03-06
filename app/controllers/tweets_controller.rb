5706e104cfc7bbce23b5dc80c910a2efe6764bd8
yasuhito1105 <yatti10@gmail.com>
2025-03-06 18:28:32 +0900

    検索機能の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -34,6 +34,10 @@ class TweetsController < ApplicationController
     @comment = Comment.new
     @comments = @tweet.comments.includes(:user)
   end
+  
+  def search
+    @tweets = Tweet.search(params[:keyword])
+  end
 
   private
   def tweet_params
